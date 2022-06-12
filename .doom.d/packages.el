0937bd0cf3001d6c16d4d16e8d695af8b5ea96fb
Sebastian Schloesser <s.schloesser@gmail.com>
2022-06-12 13:18:10 -0400

    Add emacs config

---
+++ .doom.d/packages.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+(package! company-sourcekit)
+(package! undo-tree)
+(package! org-noter)
+(package! org-roam
+          :recipe (:host github :repo "jethrokuan/org-roam"))
+(package! company-org-roam
+  :recipe (:host github :repo "jethrokuan/company-org-roam"))
+(package! lsp-sourcekit)
